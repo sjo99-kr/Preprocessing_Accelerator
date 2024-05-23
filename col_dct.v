19206557ffebb93b9c77a3b9b8809af91c9f5da5
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-05-23 14:26:07 +0900

    Update col_dct.v

--- col_dct.v
+++ col_dct.v
@@ -19,6 +19,8 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+// input : singed 8-bits 
+// output : singed 12-bits
 
 module col_dct(
     input i_clk,
@@ -35,72 +37,62 @@ module col_dct(
     
     output o_valid,
     // reflects 1d-dct result 
-    output signed [10:0] o_data0,
-    output signed [10:0] o_data1,
-    output signed [10:0] o_data2,
-    output signed [10:0] o_data3,
-    output signed [10:0] o_data4,
-    output signed [10:0] o_data5,
-    output signed [10:0] o_data6,
-    output signed [10:0] o_data7
+    output signed [11:0] o_data0,
+    output signed [11:0] o_data1,
+    output signed [11:0] o_data2,
+    output signed [11:0] o_data3,
+    output signed [11:0] o_data4,
+    output signed [11:0] o_data5,
+    output signed [11:0] o_data6,
+    output signed [11:0] o_data7
     );
     // 
     
     //stage 1 data  , (OUTPUT : 10BIT + 4BIT) 14bit for calculation
-    reg signed [14:0] temp1_data0;
-    reg signed [14:0] temp1_data1;
-    reg signed [14:0] temp1_data2;
-    reg signed [14:0] temp1_data3;
-    reg signed [14:0] temp1_data4;
-    reg signed [14:0] temp1_data5;
-    reg signed [14:0] temp1_data6;
-    reg signed [14:0] temp1_data7;
+    reg signed [15:0] temp1_data0;
+    reg signed [15:0] temp1_data1;
+    reg signed [15:0] temp1_data2;
+    reg signed [15:0] temp1_data3;
+    reg signed [15:0] temp1_data4;
+    reg signed [15:0] temp1_data5;
+    reg signed [15:0] temp1_data6;
+    reg signed [15:0] temp1_data7;
     
     // connect s1-s2 data    
     //stage 2 data
-    reg signed [14:0] temp2_data0;
-    reg signed [14:0] temp2_data1;
-    reg signed [14:0] temp2_data2;
-    reg signed [14:0] temp2_data3;
-    reg signed [14:0] temp2_data4;
-    reg signed [14:0] temp2_data5;
-    reg signed [14:0] temp2_data6;
-    reg signed [14:0] temp2_data7;
+    reg signed [15:0] temp2_data0;
+    reg signed [15:0] temp2_data1;
+    reg signed [15:0] temp2_data2;
+    reg signed [15:0] temp2_data3;
+    reg signed [15:0] temp2_data4;
+    reg signed [15:0] temp2_data5;
+    reg signed [15:0] temp2_data6;
+    reg signed [15:0] temp2_data7;
 
     //stage 3 data
-    reg signed [14:0] temp3_data0;
-    reg signed [14:0] temp3_data1;
-    reg signed [14:0] temp3_data2;
-    reg signed [14:0] temp3_data3;
-    reg signed [14:0] temp3_data4;
-    reg signed [14:0] temp3_data5;
-    reg signed [14:0] temp3_data6;
-    reg signed [14:0] temp3_data7;
+    reg signed [15:0] temp3_data0;
+    reg signed [15:0] temp3_data1;
+    reg signed [15:0] temp3_data2;
+    reg signed [15:0] temp3_data3;
+    reg signed [15:0] temp3_data4;
+    reg signed [15:0] temp3_data5;
+    reg signed [15:0] temp3_data6;
+    reg signed [15:0] temp3_data7;
 
     //stage 4 data
-    reg signed [14:0] temp4_data0;
-    reg signed [14:0] temp4_data1;
-    reg signed [14:0] temp4_data2;
-    reg signed [14:0] temp4_data3;
-    reg signed [14:0] temp4_data4;
-    reg signed [14:0] temp4_data5;
-    reg signed [14:0] temp4_data6;
-    reg signed [14:0] temp4_data7;
+    reg signed [15:0] temp4_data0;
+    reg signed [15:0] temp4_data1;
+    reg signed [15:0] temp4_data2;
+    reg signed [15:0] temp4_data3;
+    reg signed [15:0] temp4_data4;
+    reg signed [15:0] temp4_data5;
+    reg signed [15:0] temp4_data6;
+    reg signed [15:0] temp4_data7;
     
-    //stage 5 data
-    reg signed [14:0] temp5_data0;
-    reg signed [14:0] temp5_data1;
-    reg signed [14:0] temp5_data2;
-    reg signed [14:0] temp5_data3;
-    reg signed [14:0] temp5_data4;
-    reg signed [14:0] temp5_data5;
-    reg signed [14:0] temp5_data6;
-    reg signed [14:0] temp5_data7;
-    
-    
+
     
     // Pipeline stage valid
-    reg s1_valid,s2_valid, s3_valid, s4_valid, s5_valid, s6_valid;
+    reg s1_valid,s2_valid, s3_valid, s4_valid;
     
     // stage 1 
     always@(posedge i_clk)begin
@@ -142,12 +134,12 @@ module col_dct(
         end
         else begin
             if(s1_valid)begin
-                temp2_data0 <= (temp1_data3 << 4) + (temp1_data1 << 4) ;
+                temp2_data0 <= (temp1_data3 << 4) + (temp1_data0 << 4) ;
                 temp2_data1 <= (temp1_data2 << 4)  + (temp1_data1 << 4);
                 temp2_data2 <= (temp1_data1 << 4)  -  (temp1_data2 << 4);
                 temp2_data3 <= (temp1_data0 << 4)  - (temp1_data3 << 4);
                 temp2_data4 <= (temp1_data4 << 4);
-                temp2_data5 <= (temp1_data5 << 4);
+                temp2_data5 <= ((((temp1_data5 * 6)) + (temp1_data6 << 4)) * 5 / 8) - (temp1_data5 << 4);
                 temp2_data6 <= ((temp1_data5 * 6))  + (temp1_data6 << 4);
                 temp2_data7 <= (temp1_data7 << 4);
             end
@@ -172,8 +164,8 @@ module col_dct(
                 temp3_data1 <= temp2_data1;
                 temp3_data2 <= temp2_data2 - ((temp2_data3 * 3 ) / 8);
                 temp3_data3 <= temp2_data3;
-                temp3_data4 <= temp2_data4;
-                temp3_data5 <= ((temp2_data6 * 5) / 8) - temp2_data5;
+                temp3_data4 <= temp2_data4 + temp2_data5;
+                temp3_data5 <= temp2_data4 - temp2_data5;
                 temp3_data6 <= temp2_data7 - temp2_data6;
                 temp3_data7 <= temp2_data6 + temp2_data7;
             end
@@ -198,39 +190,14 @@ module col_dct(
                 temp4_data1 <= (temp3_data0  / 2 ) - temp3_data1;
                 temp4_data2 <= temp3_data2;
                 temp4_data3 <= temp3_data3 + ((temp3_data2 * 3) / 8);
-                temp4_data4 <= temp3_data4 + temp3_data5 - (temp3_data7 / 8);
-                temp4_data5 <= temp3_data4 - temp3_data5 + (temp3_data6 * 7 / 8);
-                temp4_data6 <= temp3_data6;
+                temp4_data4 <= temp3_data4 - (temp3_data7 / 8);
+                temp4_data5 <= temp3_data5 + (temp3_data6 * 7 / 8);
+                temp4_data6 <= temp3_data6 - ((temp3_data5 + (temp3_data6 * 7 / 8)) / 2);
                 temp4_data7 <= temp3_data7;
             end
         end
     end
     
-        // stage 5
-    always@(posedge i_clk)begin
-        if(i_rst)begin
-            temp5_data0 <= 0;
-            temp5_data1 <= 0;
-            temp5_data2 <= 0;
-            temp5_data3 <= 0;
-            temp5_data4 <= 0;
-            temp5_data5 <= 0;
-            temp5_data6 <= 0;
-            temp5_data7 <= 0;        
-        end
-        else begin
-            if(s4_valid) begin
-                temp5_data0 <= temp4_data0;
-                temp5_data1 <= temp4_data1;
-                temp5_data2 <= temp4_data2;
-                temp5_data3 <= temp4_data3;
-                temp5_data4 <= temp4_data4; 
-                temp5_data5 <= temp4_data5;
-                temp5_data6 <= temp4_data6 - (temp4_data5 /2 );
-                temp5_data7 <= temp4_data7;
-            end
-        end
-    end
 
     
     // output signal setting
@@ -240,24 +207,22 @@ module col_dct(
             s2_valid <= 0;
             s3_valid <= 0;
             s4_valid <= 0;
-            s5_valid <= 0;
         end
         else begin
             s1_valid <= i_valid;
             s2_valid <= s1_valid;
             s3_valid <= s2_valid;
             s4_valid <= s3_valid;
-            s5_valid <= s4_valid;
         end
     end
     
-    assign o_valid = s5_valid;
-    assign o_data0 = (temp5_data0[3:0] > 7) ? temp5_data0[14:4] + 1 :  temp5_data0[14:4];
-    assign o_data1 = (temp5_data7[3:0] > 7) ? temp5_data7[14:4] + 1 : temp5_data7[14:4];
-    assign o_data2 = (temp5_data3[3:0] > 7) ? temp5_data3[14:4] + 1: temp5_data3[14:4];
-    assign o_data3 = (temp5_data6[3:0] > 7) ? temp5_data6[14:4] + 1 : temp5_data6[14:4];
-    assign o_data4 = (temp5_data1[3:0] > 7) ? temp5_data1[14:4] + 1 : temp5_data1[14:4];
-    assign o_data5 = (temp5_data5[3:0] > 7) ? temp5_data5[14:4] + 1 : temp5_data5[14:4];
-    assign o_data6 = (temp5_data2[3:0] > 7) ? temp5_data2[14:4] + 1 : temp5_data2[14:4];
-    assign o_data7 = (temp5_data4[3:0] > 7) ? temp5_data4[14:4] + 1 : temp5_data4[14:4];
+    assign o_valid = s4_valid;
+    assign o_data0 = (temp4_data0[3:0] > 7) ? temp4_data0[15:4] + 1 : temp4_data0[15:4];
+    assign o_data1 = (temp4_data7[3:0] > 7) ? temp4_data7[15:4] + 1 : temp4_data7[15:4];
+    assign o_data2 = (temp4_data3[3:0] > 7) ? temp4_data3[15:4] + 1:  temp4_data3[15:4];
+    assign o_data3 = (temp4_data6[3:0] > 7) ? temp4_data6[15:4] + 1 : temp4_data6[15:4];
+    assign o_data4 = (temp4_data1[3:0] > 7) ? temp4_data1[15:4] + 1 : temp4_data1[15:4];
+    assign o_data5 = (temp4_data5[3:0] > 7) ? temp4_data5[15:4] + 1 : temp4_data5[15:4];
+    assign o_data6 = (temp4_data2[3:0] > 7) ? temp4_data2[15:4] + 1 : temp4_data2[15:4];
+    assign o_data7 = (temp4_data4[3:0] > 7) ? temp4_data4[15:4] + 1 : temp4_data4[15:4];
 endmodule
