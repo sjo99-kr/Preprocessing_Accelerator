5e61a0eda901ad49f100ba4cb6ab5a46300b6e60
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-07-09 16:16:10 +0900

    Update and rename kronecker.v to rtl/kronecker.v

--- rtl/kronecker.v
+++ rtl/kronecker.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2024/05/29 00:21:48
-// Design Name: 
-// Module Name: kronecker
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module kronecker(
     input i_clk,
