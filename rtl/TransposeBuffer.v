f0376b0873120018279f01f76644e5771bb3187b
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-07-09 16:15:42 +0900

    Update and rename TransposeBuffer.v to rtl/TransposeBuffer.v

--- rtl/TransposeBuffer.v
+++ rtl/TransposeBuffer.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2024/05/22 16:34:44
-// Design Name: 
-// Module Name: TransposeBuffer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module TransposeBuffer(
     input i_clk,
